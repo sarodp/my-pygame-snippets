01fef018fdc622dbb585906f0f523b35efbfd04e
sarodp <sarodp@yahoo.com>
2017-04-11 07:15:30 +0700

    Add files via upload

---
+++ pgchinese.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# https://github.com/OptimalBits/vre/blob/master/test/fonts/Cyberbit.ttf
+
+import pygame, sys
+
+
+unistr = u"黒澤 明"
+pygame.font.init()
+srf = pygame.display.set_mode((640,480))
+f = pygame.font.Font("Cyberbit.ttf",20)
+srf.blit(f.render(unistr,True,(0,0,0)),(0,0))
+pygame.display.flip()
+
+while True:
+    srf.blit(f.render(unistr,True,(255,255,255)),(0,0))
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
